25961990d86a12c785c536a64107be0a5a8477cd
Melkysedeke <melkybahia88@gmail.com>
2023-09-01 11:08:50 -0300

    Update de teste

--- Amazonia/main.js
+++ Amazonia/main.js
@@ -3,3 +3,13 @@ import { initializeCart } from "./src/menuCart.js";
   
   renderizarCatalogo();
   initializeCart();
+
+  const button = document.querySelector('.button');
+
+  // Função para remover a classe de hover após o toque
+  function removeHoverClass() {
+    button.classList.remove('hover-effect');
+  }
+
+  // Adicione um ouvinte de eventos de toque no botão
+  button.addEventListener('touchstart', removeHoverClass, { passive: true });
--- Amazonia/src/menuCart.js
+++ Amazonia/src/menuCart.js
@@ -1,5 +1,16 @@
 import { catalogo } from "./utilidades.js";
 
+const idsProdutoCarrinhoComQuantidade = {
+  '1': 0,
+  '2': 0,
+  '3': 0,
+  '4': 0,
+  '5': 0,
+  '6': 0,
+  '7': 0,
+  '8': 0,
+}
+
 function openCart (){
     const overlay = document.querySelector('#carrinho');
     overlay.style.right= 0;
@@ -22,11 +33,16 @@ export function addInCart (idProduto){
     const produto = catalogo.find((p) => p.id ===idProduto)
     const containerProdutoCarrinho =document.getElementById('produtos-carrinho');
     const cartaoProdutoCarrinho = `<article class="card-carrinho">
-   <img src="assets/img/${produto.imagem}" alt="${produto.nome}">
-   <div id="info-cart">
-     <p id="name-cart">${produto.nome}</p>
-     <p id="size-cart">Tamanho M</p>
-     <p id="price-cart">$${produto.preco}</p>
+    <img src="assets/img/${produto.imagem}" alt="${produto.nome}">
+    <div id="info-cart">
+      <p id="name-cart">${produto.nome}</p>
+      <p id="size-cart">Tamanho M</p>
+      <p id="price-cart">$${produto.preco}</p>
+   </div>
+   <div id="count">
+    <button class="countButton">+</button>
+    <p id="qntProduto">${produto.id}</p>
+    <button class="countButton">-</button>
    </div>
    <button id="rem-cart"><i class="fa-solid fa-xmark"></i></i></button>
  </article>`;
