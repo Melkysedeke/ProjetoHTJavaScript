379e6826b377a6317fe17a6a0ce0df690f301f89
Melkysedeke <melkybahia88@gmail.com>
2023-09-01 22:32:58 -0300

    Update visual

--- Amazonia/src/menuCart.js
+++ Amazonia/src/menuCart.js
@@ -63,9 +63,9 @@ function desenharProdutoCarrinho (idProduto){
       <p id="price-cart">$${produto.preco}</p>
     </div>
     <div id="count">
-      <button class="countButton" id="incrementarProduto-${produto.id}">+</button>
-      <p class="countId" id="quantidade-${produto.id}">${idsProdutoCarrinhoComQuantidade[produto.id]}</p>
       <button class="countButton" id="decrementarProduto-${produto.id}">-</button>
+      <p class="countId" id="quantidade-${produto.id}">${idsProdutoCarrinhoComQuantidade[produto.id]}</p>
+      <button class="countButton" id="incrementarProduto-${produto.id}">+</button>
     </div>
     <button class="rem-cart" id="rem-cart-${produto.id}"><i class="fa-solid fa-xmark"></i></i></button>`;
 
