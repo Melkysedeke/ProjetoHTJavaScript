1440b3dfd60a4a485a802820dace777e213e8c29
Melkysedeke <melkybahia88@gmail.com>
2023-09-08 14:46:01 -0300

    Testing

--- assets/js/menuCart.js
+++ assets/js/menuCart.js
@@ -17,7 +17,7 @@ function irParaCheckout (){
     console.log("não é para ir para checkout");
     return;
   }
-  window.location.href = window.location.host + '/checkout.html';
+  window.location.href = window.location.replace('/checkout.html');
 }
 
 export function initializeCart (){
